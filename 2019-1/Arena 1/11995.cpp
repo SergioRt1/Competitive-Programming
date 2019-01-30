8494c62a4fa6330cbaf8f886d1694cc3a51f877a
SergioRt1 <sergio200035@gmail.com>
2019-01-30 00:23:56 -0500

    fix

--- 2019-1/Arena 1/11995.cpp
+++ 2019-1/Arena 1/11995.cpp
@@ -8,41 +8,47 @@ using namespace std;
 
 typedef long long large;
 
+stack<int> st;
+queue<int> qu;
+priority_queue<int> pq;
+
 int main(){
-//   freopen("11995.in","r",stdin);
-//   freopen("11995.out","w",stdout);
+   freopen("11995.in","r",stdin);
+   freopen("11995.out","w",stdout);
    string line;
    int c,v,x,n;
    while(scanf("%d",&n)==1){
       bool s=true,q=true,p=true;
-      stack<int> st;
-      queue<int> qu;
-      priority_queue<int> pq;
+      while(!st.empty())st.pop();
+      while(!qu.empty())qu.pop();
+      while(!pq.empty())pq.pop();
       getline(cin,line);
       loop(i,0,n){
-         getline(cin,line);
-         stringstream ss(line);
-         ss>>c>>v;
+         scanf("%d %d",&c,&v);
          if(c==1){
-            st.push(v);
-            qu.push(v);
-            pq.push(v);
+            if(s)st.push(v);
+            if(q)qu.push(v);
+            if(p)pq.push(v);
          }else{
-            if(s&&!st.empty())
-               s=st.top()==v;
-               st.pop();
-            if(q&&!qu.empty())
-               q=qu.front()==v;
-               qu.pop();
-            if(p&&!pq.empty())
-               p=pq.top()==v;
-               pq.pop();
+            if(s)
+               if(st.empty() || st.top() != v)
+                  s=false;
+               else st.pop();
+            if(q)
+               if(qu.empty() || qu.front() != v)
+                  q=false;
+               else qu.pop();
+
+            if(p)
+               if(pq.empty() || pq.top()!=v)
+                  p=false;
+               else pq.pop();
          }
       }
       if(!(s||q||p))puts("impossible");
       else if(q && !p && !s)puts("queue");
       else if(s && !p && !q)puts("stack");
-      else if(p && !q && !s)puts("priority queue ");
+      else if(p && !q && !s)puts("priority queue");
       else puts("not sure");
    }
    return 0;
